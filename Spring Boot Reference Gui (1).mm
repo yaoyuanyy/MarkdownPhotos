baa0bae86fbe92a1dfde03e62d41e0fa5e25b510
skyler <yaoyuanyy@users.noreply.github.com>
2019-07-30 17:29:30 +0800

    Add files via upload
    add png

---
+++ Spring Boot Reference Gui (1).mm
@@ -0,0 +1,203 @@
+<map>
+		<node ID="root" TEXT="Spring Boot Reference Guide 2.1.6">		<node TEXT="I(一). spring boot document" ID="17916c4033c4230e9" STYLE="bubble" POSITION="right">
+		<node TEXT="1. About the Documentation" ID="2f216c41f0f3cd179" STYLE="fork">
+		</node>
+		<node TEXT="2. Getting Help" ID="38e16c41f2c50e0cd" STYLE="fork">
+		</node>
+		<node TEXT="3. First Steps" ID="33d16c41f3002315d" STYLE="fork">
+		</node>
+		<node TEXT="4. Working with Spring boot" ID="29816c41f32676128" STYLE="fork">
+		</node>
+		<node TEXT="5. Learning about Spring boot Features" ID="29f16c41f35838153" STYLE="fork">
+		</node>
+		<node TEXT="6. Moving to Production" ID="21a16c41f39136127" STYLE="fork">
+		</node>
+		<node TEXT="7. Advanced Topics" ID="10516c41f3be300b8" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="II(二). Getting Started" ID="23b16c4035dc3209f" STYLE="bubble" POSITION="right">
+		<node TEXT="8. Introducing Spring boot" ID="11b16c41f3f879111" STYLE="fork">
+		</node>
+		<node TEXT="9. System Requirements" ID="37116c41f425ed085" STYLE="fork">
+		<node TEXT="9.1. Servlet Container" ID="2b816c41f44adf175" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="10. Installing Spring boot" ID="32216c41fd5dbd123" STYLE="fork">
+		<node TEXT="10.1 Installing Instructions" ID="1f816c41fded5a031" STYLE="fork">
+		</node>
+		<node TEXT="10.2 Installing Spring boot CLI" ID="2af16c41fe2cb0103" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="11. Developing Your First Spring boot Application" ID="17d16c41fe6b4e109" STYLE="fork">
+		<node TEXT="11.1 Creating the POM" ID="3c216c41ff0a9e04d" STYLE="fork">
+		</node>
+		<node TEXT="11.2 Adding Classpath Dependencies" ID="18116c41ff481d12" STYLE="fork">
+		</node>
+		<node TEXT="11.3 Writing the code" ID="416c41ff873b0f6" STYLE="fork">
+		</node>
+		<node TEXT="11.4 Running the Example" ID="1e116c41ffaa310e3" STYLE="fork">
+		</node>
+		<node TEXT="11.5 Creating an Executable Jar" ID="10316c41ffda9f042" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="12. What to Read Next" ID="8116c420032e8111" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="III(三). Using spring boot" ID="1b916c403675c403a" STYLE="bubble" POSITION="right">
+		<node TEXT="13. Build Systems" ID="12a16c42008cd2087" STYLE="fork">
+		</node>
+		<node TEXT="14. Structuring Your Code" ID="37e16c4202e33e04f" STYLE="fork">
+		</node>
+		<node TEXT="15. Configuration Classes" ID="3a216c42033baa0d" STYLE="fork">
+		</node>
+		<node TEXT="16. Auto-Configuration" ID="31b16c42038a4d0ec" STYLE="fork">
+		</node>
+		<node TEXT="17. Spring Beans and Dependency Injection" ID="2e616c4203959c14c" STYLE="fork">
+		</node>
+		<node TEXT="18. Using the @SpringBootApplication Annotation" ID="14116c4204a4ac0ec" STYLE="fork">
+		</node>
+		<node TEXT="19. Running Your Application" ID="13416c42043bd7127" STYLE="fork">
+		</node>
+		<node TEXT="20. Developer Tools" ID="33c16c42048b2610c" STYLE="fork">
+		</node>
+		<node TEXT="21. Packaging Your Application for Production" ID="27916c4205e014115" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="IV(四). spring boot features" ID="d916c4036c34f14d" STYLE="bubble" POSITION="right">
+		<node TEXT="23. SpringApplication" ID="2bf16c42067a1b0c1" STYLE="fork">
+		</node>
+		<node TEXT="24. Externalized Configuration" ID="ab16c42068f17033" STYLE="fork">
+		</node>
+		<node TEXT="25. Profiles" ID="38016c4206975a0f7" STYLE="fork">
+		</node>
+		<node TEXT="26. Logging" ID="fe16c42069e100a" STYLE="fork">
+		</node>
+		<node TEXT="27. Internationalization" ID="10f16c4206a3ec0e5" STYLE="fork">
+		</node>
+		<node TEXT="28. JSON" ID="13b16c4206ae0115" STYLE="fork">
+		</node>
+		<node TEXT="29. Developing Web Applications" ID="3b716c4206b45313f" STYLE="fork">
+		</node>
+		<node TEXT="30. Sesurity" ID="39416c4206bdcc01" STYLE="fork">
+		</node>
+		<node TEXT="31. Working with SQL Databases" ID="e216c420874a810f" STYLE="fork">
+		</node>
+		<node TEXT="32. Working with NOSQL Technologies" ID="18b16c420908b6063" STYLE="fork">
+		</node>
+		<node TEXT="33. Caching" ID="31b16c4209776d103" STYLE="fork">
+		</node>
+		<node TEXT="34. Messaging" ID="5116c4209b1e6186" STYLE="fork">
+		</node>
+		<node TEXT="35. Calling REST Services with RestTemplate   " ID="10416c4208a77c0e2" STYLE="fork">
+		</node>
+		<node TEXT="36.  Calling REST Services with WebClient" ID="25616c420a40f3149" STYLE="fork">
+		</node>
+		<node TEXT="37. Validation" ID="37116c420a6ce1028" STYLE="fork">
+		</node>
+		<node TEXT="38. Sending Email" ID="14a16c420a84b013" STYLE="fork">
+		</node>
+		<node TEXT="39. Distributed Transactions with JTA" ID="36816c420aa2a3095" STYLE="fork">
+		</node>
+		<node TEXT="40. .." ID="1f616c420af09c06d" STYLE="fork">
+		</node>
+		<node TEXT="41. Quartz Scheduler" ID="26816c420b5f80168" STYLE="fork">
+		</node>
+		<node TEXT="42. Task Execution and Scheduling" ID="14716c420b915e195" STYLE="fork">
+		</node>
+		<node TEXT="43. Spring Integration" ID="16116c420bcb59059" STYLE="fork">
+		</node>
+		<node TEXT="44. Spring Session" ID="9c16c420bf60402" STYLE="fork">
+		</node>
+		<node TEXT="45. Monitoring and Management over JMX" ID="1d16c420c174d094" STYLE="fork">
+		</node>
+		<node TEXT="46. Testing" ID="3a616c420c60cc185" STYLE="fork">
+		</node>
+		<node TEXT="47. WebSockets" ID="20c16c420c84330c" STYLE="fork">
+		</node>
+		<node TEXT="48. Web Services" ID="5616c420cbc74155" STYLE="fork">
+		</node>
+		<node TEXT="49. Creating Your Own Auto-configuration" ID="28516c420ce3e518d" STYLE="fork">
+		</node>
+		<node TEXT="50. Kotlin support" ID="2e316c420d61e218d" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="V(五). Spring Boot Actuctor: Production-ready features" ID="4a16c4037dd3a17f" STYLE="bubble" POSITION="right">
+		<node TEXT="52. Enabling Production-ready Features" ID="27116c420daea115e" STYLE="fork">
+		</node>
+		<node TEXT="53. Endpoints" ID="c916c420e9cdf126" STYLE="fork">
+		</node>
+		<node TEXT="54. Monitoring and Management over HTTP" ID="2d016c420ef4ef0c8" STYLE="fork">
+		</node>
+		<node TEXT="55. Monitoring and Management over JMX" ID="23716c420f5dd80a" STYLE="fork">
+		</node>
+		<node TEXT="56. Loggers" ID="2f716c420fa54803f" STYLE="fork">
+		</node>
+		<node TEXT="57. Metrics" ID="2a716c420fbed00a3" STYLE="fork">
+		</node>
+		<node TEXT="58. Auditing" ID="16e16c420ff5f415b" STYLE="fork">
+		</node>
+		<node TEXT="59. HTTP Tracting" ID="26d16c42100bd616b" STYLE="fork">
+		</node>
+		<node TEXT="60. Process Monitoring" ID="34816c42102ef60be" STYLE="fork">
+		</node>
+		<node TEXT="61. Cloud Foundry Support" ID="35216c42105a4917f" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="VI(六). deloying spring boot application" ID="36816c40381b320a8" STYLE="bubble" POSITION="left">
+		</node>
+		<node TEXT="VII(七). spring boot cli " ID="10d16c4038847603b" STYLE="bubble" POSITION="left">
+		</node>
+		<node TEXT="VIII(八). build tool plugins" ID="24616c4038c2430c9" STYLE="bubble" POSITION="left">
+		</node>
+		<node TEXT="IX(九). &quot;How-to&quot; guides" ID="23416c4038f3310ef" STYLE="bubble" POSITION="left">
+		<node TEXT="76. Spring Boot Application" ID="32e16c4212d18018f" STYLE="fork">
+		</node>
+		<node TEXT="77. Properties and Configuration" ID="2e216c421325560b" STYLE="fork">
+		</node>
+		<node TEXT="78. Embedded Web Servers" ID="22c16c42135246191" STYLE="fork">
+		</node>
+		<node TEXT="79. Spring MVC" ID="5816c4213a2150db" STYLE="fork">
+		</node>
+		<node TEXT="80. Testing with Spring Security" ID="1416c4213ea2e156" STYLE="fork">
+		</node>
+		<node TEXT="81. Jersey" ID="a16c4214414516b" STYLE="fork">
+		</node>
+		<node TEXT="82. HTTP Clients" ID="c416c42146548176" STYLE="fork">
+		</node>
+		<node TEXT="83. Logging" ID="10316c4214883d029" STYLE="fork">
+		</node>
+		<node TEXT="84. Data Access" ID="20516c42149d4b154" STYLE="fork">
+		</node>
+		<node TEXT="85. Database Initialization" ID="1016c4214bc4d084" STYLE="fork">
+		</node>
+		<node TEXT="86. Messaging" ID="6316c42150c8f0d4" STYLE="fork">
+		</node>
+		<node TEXT="87. Batch Appcalitions" ID="21316c421525d20f9" STYLE="fork">
+		</node>
+		<node TEXT="88. Actuator" ID="2f016c42154e3f16c" STYLE="fork">
+		</node>
+		<node TEXT="89. Security" ID="17f16c421564aa09a" STYLE="fork">
+		</node>
+		<node TEXT="90. Hot Swapping" ID="27716c42157b010f4" STYLE="fork">
+		</node>
+		<node TEXT="91. Build" ID="6a16c4215ca99171" STYLE="fork">
+		</node>
+		<node TEXT="92. Traditional Deployment" ID="29616c4215dd3206f" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="X(十). Appendices" ID="3cb16c40396960093" STYLE="bubble" POSITION="left">
+		<node TEXT="A. Common application properties" ID="6616c42161ad80af" STYLE="fork">
+		</node>
+		<node TEXT="B. Configuration MetaData" ID="3f16c42168e4d12c" STYLE="fork">
+		</node>
+		<node TEXT="C. Auto-configuration classes" ID="29e16c4216c80717e" STYLE="fork">
+		</node>
+		<node TEXT="D. Test auto-configuration annotatioins" ID="1d516c42172b9b073" STYLE="fork">
+		</node>
+		<node TEXT="E. The Executable Jar Format" ID="ab16c4217747f136" STYLE="fork">
+		</node>
+		<node TEXT="F. Dependency versions" ID="19f16c4217e5b7164" STYLE="fork">
+		</node>
+		</node>
+</node>
+</map>
